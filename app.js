5c5965ceee2cac40a53845b8622a99e36fbf07f8
mariogadan <mario.gadan@gmail.com>
2022-09-07 14:42:47 -0300

    Subo proyecto

---
+++ app.js
@@ -0,0 +1,23 @@
+// requiero modulos y cargo express
+const express = require('Express');
+const path = require('path');
+const app = express();
+
+// levanto servidor y confirmo con mensaje
+app.listen(process.env.PORT || 3000, function() {console.log('Servidor corriendo');}) 
+
+
+// defino ruta de archivos estaticos y genero rutas
+app.use(express.static(path.join(__dirname,'./public')));
+
+app.get('/home', function(req,res){
+    res.sendFile(path.join(__dirname,'./views/home.html'));
+});
+
+app.get('/register', function(req,res){
+    res.sendFile(path.join(__dirname,'./views/register.html'));
+});
+
+app.get('/login', function(req,res){
+    res.sendFile(path.join(__dirname,'./views/login.html'));
+});
