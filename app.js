3597c5ab63202246be7dcec8feca25e49ae74009
mariogadan <mario.gadan@gmail.com>
2022-09-07 14:55:57 -0300

    Arreglo app.js

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const express = require('Express');
 const path = require('path');
 const app = express();
 
+
+app.use(express.static(path.join(__dirname,'./public')));
 // levanto servidor y confirmo con mensaje
 app.listen(process.env.PORT || 3000, function() {console.log('Servidor corriendo');}) 
 
 
 // defino ruta de archivos estaticos y genero rutas
-app.use(express.static(path.join(__dirname,'./public')));
-
 app.get('/home', function(req,res){
     res.sendFile(path.join(__dirname,'./views/home.html'));
 });
